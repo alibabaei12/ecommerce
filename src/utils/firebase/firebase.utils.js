6b3364664087bbdcacdce01aa52acfe2b4ff7115
Ali Babaei <alibabaei@Alis-MacBook-Pro.local>
2023-03-31 17:27:31 -0700

    added sign in component as well as firebase utils and libs

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,60 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQlnoBt4KZ_zz531ahsDPgNIM8vWJEP2Y",
+  authDomain: "ecommerce-ae349.firebaseapp.com",
+  projectId: "ecommerce-ae349",
+  storageBucket: "ecommerce-ae349.appspot.com",
+  messagingSenderId: "595619358632",
+  appId: "1:595619358632:web:90fc7ef7aeebd213c01f9d",
+  //   measurementId: "G-BFGRCZJBHZ"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+g
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user: ", error.message);
+    }
+  }
+
+  return userDocRef;
+};
